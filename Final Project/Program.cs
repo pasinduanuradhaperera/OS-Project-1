b89f879702803d82a06bff0138e89ddbc355851d
Waruna Dilshan <121342669+WarunaDilshan@users.noreply.github.com>
2023-03-15 09:17:21 +0530

    Update Program.cs

--- Final Project/Program.cs
+++ Final Project/Program.cs
@@ -9,7 +9,7 @@ namespace Final_Project
     internal static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// the main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
