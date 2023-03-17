ed717961d0bf33d47f773b3c4391845f39cd810e
AshwidaNethmina <67303584+AshwidaNethmina@users.noreply.github.com>
2023-03-17 12:25:46 +0530

    Update Program.cs

--- Final Project/Program.cs
+++ Final Project/Program.cs
@@ -17,9 +17,6 @@ namespace Final_Project
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            
-            Console.Write("summary");
-
         }
     }
 }
